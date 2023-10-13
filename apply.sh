e1dd512df73e509fa16e27aa452295b14fa89304
Sem Reiestad <afotball@gmail.com>
2023-10-13 15:13:55 +0200

    Applying kube

---
+++ apply.sh
@@ -0,0 +1,5 @@
+kubectl apply -f app-data-persistentvolumeclaim.yaml
+kubectl apply -f db-deployment.yaml
+kubectl apply -f db-service.yaml
+kubectl apply -f app-deployment.yaml
+kubectl apply -f app-service.yaml
